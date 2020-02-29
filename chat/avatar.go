ff8a4bdb8419b3dbbc359be6b2ba7e3e6f794a3b
Tattsum <kurohari35@gmail.com>
2020-02-29 13:05:03 +0900

    認証サービスを使ったAvatarの実装

--- chat/avatar.go
+++ chat/avatar.go
@@ -13,3 +13,16 @@ type Avatar interface {
 	// 問題が発生した場合にはエラーを返します．特にURLを取得できなかった場合には，ErrNoAvatarURLを返します．
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
