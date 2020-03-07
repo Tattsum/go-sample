2eff40667f247e8c953277b6dcb51b7dc6873296
Tattsum <kurohari35@gmail.com>
2020-03-07 13:26:40 +0900

    他形式のファイルへの対応

--- chat/avatar.go
+++ chat/avatar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
+	"path/filepath"
 )
 
 // ErrNoAavatarはAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラーです．
@@ -47,7 +49,16 @@ var UseFileSystemAvatar FileSystemAvatar
 func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok {
-			return "/avatars/" + useridStr + ".jpg", nil
+			if files, err := ioutil.ReadDir("avatars"); err == nil {
+				for _, file := range files {
+					if file.IsDir() {
+						continue
+					}
+					if match, _ := filepath.Match(useridStr+"*", file.Name()); match {
+						return "/avatars/" + file.Name(), nil
+					}
+				}
+			}
 		}
 	}
 	return "", ErrNoAvatarURL
