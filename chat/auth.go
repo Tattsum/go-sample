15e3d1febbb980a44ebff7830a2e6c23e5173907
Tattsum <kurohari35@gmail.com>
2020-03-07 12:57:14 +0900

    訂正

--- chat/auth.go
+++ chat/auth.go
@@ -69,7 +69,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		m := md5.New()
-		io.WriteString(m, strings.ToLower(user.Name()))
+		io.WriteString(m, strings.ToLower(user.Email()))
 		userID := fmt.Sprintf("%x", m.Sum(nil))
 
 		// データを保存します．
