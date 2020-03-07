329b314df362745233df62dd8498685b6b1091f5
Tattsum <kurohari35@gmail.com>
2020-03-07 14:05:10 +0900

    具象型をインタフェースに置き換える

--- chat/auth.go
+++ chat/auth.go
@@ -11,6 +11,20 @@ import (
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/objx"
 )
+import gomniauthcommon "github.com/stretchr/gomniauth/common"
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
 
 type authHandler struct {
 	next http.Handler
