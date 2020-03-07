185f9d08e30b8b650af86d91c0570c5c7ea888ee
Tattsum <kurohari35@gmail.com>
2020-03-07 14:11:32 +0900

    TDDに基づいたインタフェースの変更 失敗

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -9,16 +9,19 @@ import (
 
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
-	client := new(client)
-	url, err := authAvatar.GetAvatarURL(client)
+	testUser := &gomniauthtest.TestUser{}
+	testUser.on("AvatarURL").Return("", ErrNoAvatarURL)
+	testChatUser := &chatUser{User: testUser}
+	url, err := authAvatar.GetAvatarURL(testChatUser)
 	if err != ErrNoAvatarURL {
 		t.Error("値が存在しない場合，AuthAvatar.GetAvatarURLは" + "ErrNoAvatarURLを返すべきです．")
 	}
-
 	//値をセットします．
 	testUrl := "http://url-to-avatar/"
-	client.userData = map[string]interface{}{"avatar_url": testUrl}
-	url, err = authAvatar.GetAvatarURL(client)
+	testUser = &gomniauthtest.TestUser{}
+	testChatUser.User = testUser
+	testUser.on("AvatarURL").Return(testUrl, nil)
+	url, err = authAvatar.GetAvatarURL(testChatUser)
 	if err != nil {
 		t.Error("値が存在しない場合，AuthAvatar.GetAvatarURLは" + "ErrNoAvatarURLを返すべきです．")
 	} else {
